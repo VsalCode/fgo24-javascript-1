454c614b0321d0e65305231d215e24581fa1e98a
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-16 14:37:29 +0700

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,3 +1,8 @@
+/*----------------------------------------------- 
+FOR LOOP
+-----------------------------------------------*/
+
+// FAZZTRACK
 for (let index = 1; index <= 5; index++) {
   if ( index == 4 ){
     console.log("Fazztrack");
@@ -7,13 +12,48 @@ for (let index = 1; index <= 5; index++) {
   }
 }
 
-/* output :
-1
-2
-3
-Fazztrack
-5
-*/
+// FOR - SEGITIGA TERBALIK
+
+for (let r1 = 0; r1 < 5; r1++) {
+  row = ""
+  for (let r2 = 0; r2 < 5 - r1; r2++) {
+    row += "* "
+  }  
+  console.log(row);
+  
+}
+
+// FOR - SEGITIGA SIKU-SIKU
+for (let r1 = 0; r1 <= 5; r1++) {
+  row = ""
+  for (let r2 = 0; r2 < r1; r2++) {
+    row += "* "
+  }  
+  console.log(row);
+  
+}
+
+/*----------------------------------------------- 
+WHILE LOOP
+-----------------------------------------------*/
+
+// FAZZTRACK
+let index1 = 0
+
+while(index1 < 1){
+  index2 = 0
+  while(index2 <= 5){
+    if(index2 == 4){
+      console.log("Fazztrack");
+    } else {
+      console.log(index2);
+    }
+    index2++
+  }
+  index1++
+}
+
+// WHILE - SEGITIGA TERBALIK
 
 let x = 0
 
@@ -28,15 +68,7 @@ while (x < 5) {
   x++;
 } 
 
-/* output :
-
-* * * * * 
- * * * * 
- * * * 
- * * 
- * 
-
-*/
+// WHILE - SEGITIGA SIKU-SIKU
 
 let a = 0
 
@@ -51,12 +83,20 @@ while (a <= 5) {
   a++;
 }
 
-/* Output :
+/*----------------------------------------------- 
+DO WHILE LOOP
+-----------------------------------------------*/
 
- * 
- * * 
- * * * 
- * * * * 
- * * * * * 
+// FAZZTRACK
 
-*/
+z = 1
+
+do {
+  if(z == 4){
+    console.log("Fazztrack");
+  } else {
+    console.log(z);
+    
+  }
+  z++
+} while (z <= 5); 
