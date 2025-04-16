215a8870b8232a6a2bf7d5a333d327f66c343956
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-16 10:26:44 +0700

    Update access-arrays.js

--- access-arrays.js
+++ access-arrays.js
@@ -1,5 +1,5 @@
-console.log(data);
-
 const data = [
-  ["nol",["nol","satu","dua",{ number: 123, String: ["random", { value: "Hello"}] }]], "lorem","ipsum"
+  ["nol",["nol","satu","dua",{ number: 123, string: ["random", { value: "Hello"}] }]], "lorem","ipsum"
 ]
+
+console.log(data[0][1][3].string[1].value);
